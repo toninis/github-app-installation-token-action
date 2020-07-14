8d8bfd8ae757365b110074960772a179a6778002
jon wong <j@jnwng.com>
2020-07-13 21:43:06 -0700

    Add integration test for installation token action

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,27 @@
+import {Octokit} from '@octokit/rest'
+import {AppsCreateInstallationAccessTokenResponseData} from '@octokit/types'
+import {createAppAuth} from '@octokit/auth-app'
 import * as core from '@actions/core'
-import {wait} from './wait'
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`)
+    const appId = core.getInput('appId')
+    const installationId = core.getInput('installationId')
+    const privateKey = core.getInput('privateKey')
+    const octokit = new Octokit({
+      authStrategy: createAppAuth,
+      auth: {
+        id: appId,
+        privateKey
+      }
+    })
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    const {token} = (await octokit.auth({
+      type: 'installation',
+      installationId
+    })) as AppsCreateInstallationAccessTokenResponseData
 
-    core.setOutput('time', new Date().toTimeString())
+    core.setOutput('token', token)
   } catch (error) {
     core.setFailed(error.message)
   }
