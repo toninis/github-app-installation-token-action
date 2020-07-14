98aeae88364a272e83eb6982dfdd0717df51a907
jon wong <j@jnwng.com>
2020-07-13 22:16:44 -0700

    Adding tests for token retrieval

--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,13 @@
-import {Octokit} from '@octokit/rest'
-import {AppsCreateInstallationAccessTokenResponseData} from '@octokit/types'
-import {createAppAuth} from '@octokit/auth-app'
 import * as core from '@actions/core'
+import {getToken} from './get-token'
 
 async function run(): Promise<void> {
   try {
     const appId = core.getInput('appId')
     const installationId = core.getInput('installationId')
     const privateKey = core.getInput('privateKey')
-    const octokit = new Octokit({
-      authStrategy: createAppAuth,
-      auth: {
-        id: appId,
-        privateKey
-      }
-    })
 
-    const {token} = (await octokit.auth({
-      type: 'installation',
-      installationId
-    })) as AppsCreateInstallationAccessTokenResponseData
+    const {token} = getToken({appId, installationId, privateKey})
 
     core.setOutput('token', token)
   } catch (error) {
